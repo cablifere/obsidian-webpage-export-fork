542c6b386b8852ab67e3ced06b80e681970a6ce4
cablifere <me@bibingka.dev>
2025-08-10 23:23:53 +0800

    1.9.1 upstream changes

--- src/frontend/main/links.ts
+++ src/frontend/main/links.ts
@@ -24,7 +24,7 @@ export class LinkHandler {
 
   public static getPathnameFromURL(url: string): string {
     if (url === "" || url === "/" || url === "\\") {
-      return "/index.html";
+      return "index.html";
     }
     if (url?.startsWith("#") || url?.startsWith("?")) {
       return (ObsidianSite.document?.pathname?.split("#")[0]?.split("?")[0] ?? "") + (url ?? "");
--- src/plugin/render-api/render-api.ts
+++ src/plugin/render-api/render-api.ts
@@ -561,7 +561,7 @@ export namespace _MarkdownRendererInternal {
       const fileCache = ObsidianApp.app.metadataCache.getFileCache(file);
       const frontmatter = fileCache?.frontmatter;
       const titleFromFrontmatter = frontmatter?.[Settings.exportOptions.titleProperty] ?? frontmatter?.["banner_header"]; // banner plugin support
-      title = (titleFromFrontmatter ?? file.basename).toString() ?? "";
+      title = (titleFromFrontmatter ?? file.basename)?.toString() ?? "";
 
       if (title.endsWith(".excalidraw")) {
         title = title.substring(0, title.length - 11);
